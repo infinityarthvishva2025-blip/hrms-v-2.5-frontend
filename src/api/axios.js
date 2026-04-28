1a7051886c8b55159b05eff13edf3818e566fdd2
madhavmore23445-saka <madhavmore23445@gmail.com>
2026-04-28 12:45:10 +0530

    fixed the acces issues

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,6 +4,7 @@ const API_BASE_URL = import.meta.env.VITE_API_URL || '/api';
 
 
 
+
 const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  
   server: {
     port: 5173,
+    host: true,
+    allowedHosts: ['hrms-v-2-5-frontend.onrender.com'],
     proxy: {
       '/api': {
-        target: 'http://192.168.1.45:5000',
+        target: 'https://hrms-v-2-5-backend.vercel.app/',
         changeOrigin: true,
       },
     },
   },
+
+  preview: {
+    host: true,
+    allowedHosts: ['hrms-v-2-5-frontend.onrender.com'],
+  }
 })
